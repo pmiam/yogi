ae6d7e03fa703f659ec6060fcc48aba06dd9f1b0
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-04-16 19:34:51 -0400

    configured convenience imports

--- yogi/__init__.py
+++ yogi/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '0.1.0'
+
+from . import model_selection
+from . import metrics
+
+__all__ = ["model_selection", "metrics"]
---
+++ yogi/model_selection/__init__.py
@@ -0,0 +1,7 @@
+from .hpo_help import summarize_HPO, collect_top_rankings
+from .pandas_validation_curve import pandas_validation_curve
+
+__all__ = ["summarize_HPO",
+           "collect_top_rankings",
+           "pandas_validation_curve"] 
+ 
