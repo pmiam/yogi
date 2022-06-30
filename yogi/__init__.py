de73e401244d19a19cfa357b76fea387cab5c613
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-06-30 13:09:31 -0400

    compatsk init imports

--- yogi/__init__.py
+++ yogi/__init__.py
@@ -3,5 +3,9 @@ __version__ = '0.1.1'
 from . import data
 from . import model_selection
 from . import metrics
+from . import compatsk
 
-__all__ = ["data", "model_selection", "metrics"]
+__all__ = ["data",
+           "model_selection",
+           "metrics",
+           "compatsk"]
