4c702aa1734e7e35d17e0b9c22f9b44712ac027b
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-07-07 17:16:19 -0400

    metrics namespace

---
+++ yogi/metrics/__init__.py
@@ -0,0 +1,8 @@
+from .pandas_scoring import PandasScoreAdaptor
+from .pandas_scoring import batch_score, test_generality
+
+__all__ = [
+    "PandasScoreAdaptor",
+    "batch_score",
+    "test_generality",
+]
